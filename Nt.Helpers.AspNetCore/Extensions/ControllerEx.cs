5d4463b0b919d519b1b5cbe4176128edaf5b52f0
nikita titarenko <nt@poloniumarts.com>
2018-08-15 15:52:37 +0300

    + DefaultApiOk()

--- Nt.Helpers.AspNetCore/Extensions/ControllerEx.cs
+++ Nt.Helpers.AspNetCore/Extensions/ControllerEx.cs
@@ -5,6 +5,7 @@ using System.Text;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 using Nt.Helpers.AspNetCore.Models;
 
 namespace Nt.Helpers.AspNetCore.Extensions
@@ -12,31 +13,37 @@ namespace Nt.Helpers.AspNetCore.Extensions
 
     public static class ControllerEx
     {
-        static readonly JsonSerializerSettings _defaults = new JsonSerializerSettings
+        public static readonly JsonSerializerSettings SerializationSettings = new JsonSerializerSettings
         {
             ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
             PreserveReferencesHandling = PreserveReferencesHandling.None,
             //Converters = new List<JsonConverter> { new DateTimeGlobalConverter() },
             NullValueHandling = NullValueHandling.Ignore,
+            //ContractResolver = new CamelCasePropertyNamesContractResolver()
             // todo NT: Олег просил вернуть пустые массивы
             //ContractResolver = new ContractResolver(true, true)
         };
 
+        public static IActionResult DefaultApiOk(this Controller controller)
+        {
+            return controller.Json(new RtApiResponse<string>(string.Empty), SerializationSettings);
+        }
+
         public static IActionResult ApiOk<TData>(this Controller controller, TData data)
         {
-            return controller.Json(new RtApiResponse<TData>(data), _defaults);
+            return controller.Json(new RtApiResponse<TData>(data), SerializationSettings);
         }
 
         public static IActionResult ApiError(this Controller controller, string error)
         {
             var response = new RtApiErrorResponse(error);
-            return controller.Json(response, _defaults);
+            return controller.Json(response, SerializationSettings);
         }
 
         public static IActionResult ApiError(this Controller controller, Exception ex)
         {
             var response = new RtApiErrorResponse(ex);
-            return controller.Json(response, _defaults);
+            return controller.Json(response, SerializationSettings);
         }
 
         public static IActionResult ApiError(this Controller controller, ModelStateDictionary modelState)
@@ -44,7 +51,7 @@ namespace Nt.Helpers.AspNetCore.Extensions
             var errList = GetModelStateErrors(modelState);
             var response = new RtApiErrorResponse(errList);
 
-            return controller.Json(response, _defaults); ;
+            return controller.Json(response, SerializationSettings); ;
         }
 
         /// <summary>
